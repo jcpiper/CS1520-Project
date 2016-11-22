9ffbed3025a40e9a33fdf80c7510d9bd8458b1ec
Justin Piper <jpiper4198@gmail.com>
2016-11-22 14:10:59 -0500

    removed debugging code and added amazon links

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 			"abt" => "#about",
 			"prj" => "#project",
 			"bks" => "#books",
-			"ml" => "#mailingList"
+			"ml" => "#bookSubmission"
 		);
 	?>
 
@@ -37,7 +37,7 @@
 					<a href=<?php echo $links["bks"]; ?>>Book Recommendations</a>
 				</li>
 				<li>
-					<a href=<?php echo $links["ml"]; ?>>Mailing List</a>
+					<a href=<?php echo $links["ml"]; ?>>Book Submission</a>
 				</li>
 			</ul>
 		</nav>
@@ -64,6 +64,19 @@
       by their students.
     </p>
 	</section>
+  <?php
+    $amazon = array(
+      "asimov" => "https://www.amazon.com/Foundation-Empire-Second-Everymans-Library/dp/0307593967/ref=la_B003RY2ISS_1_2?s=books&ie=UTF8&qid=1479839454&sr=1-2&refinements=p_82%3AB003RY2ISS%2Cp_n_feature_browse-bin%3A2656020011",
+      "mark" => "https://www.amazon.com/Familiar-One-Rainy-Day-May/dp/0375714944/ref=sr_1_1?s=books&ie=UTF8&qid=1479839620&sr=1-1&keywords=the+familiar",
+      "heinlein" => "https://www.amazon.com/Starship-Troopers-Robert-Heinlein/dp/0441783589/ref=sr_1_1?s=books&ie=UTF8&qid=1479839649&sr=1-1&keywords=starship+troopers",
+      "watch" => "https://www.amazon.com/Watchmen-Alan-Moore/dp/1401245250/ref=sr_1_1?s=books&ie=UTF8&qid=1479839689&sr=1-1&keywords=watchmen",
+      "v" => "https://www.amazon.com/V-Vendetta-Alan-Moore/dp/140120841X/ref=sr_1_1?s=books&ie=UTF8&qid=1479839718&sr=1-1&keywords=v+for+vendetta",
+      "portis" => "https://www.amazon.com/True-Grit-Novel-Charles-Portis/dp/159020459X/ref=sr_1_1?s=books&ie=UTF8&qid=1479839761&sr=1-1&keywords=true+grit",
+      "proulx" => "https://www.amazon.com/Close-Range-Wyoming-Annie-Proulx/dp/0684852225/ref=sr_1_1?s=books&ie=UTF8&qid=1479839798&sr=1-1&keywords=close+range+wyoming+stories",
+      "alexie" => "https://www.amazon.com/Ranger-Tonto-Fistfight-Heaven-Anniversary/dp/0802121993/ref=sr_1_1?s=books&ie=UTF8&qid=1479839836&sr=1-1&keywords=the+lone+ranger+and+tonto+fistfight+in+heaven"
+    );
+    $tab = "target=\"_blank\"";
+  ?>
 	<section id="books">
 		<h1>Book Recommendations</h1>
 		<p>
@@ -78,62 +91,51 @@
       <!--add pics of each author that appear on mouse over-->
       <li>
 		  	<p id="asimov">
-					The Foundation Trilogy - Isaac Asimov
+					The Foundation Trilogy - <a href=<?php echo "\"". $amazon["asimov"] . "\", " . $tab; ?>>Isaac Asimov</a>
 				</p>
       </li>
       <li>
 				<p id="mark">
-					The Familiar, Vol. One: One Rainy Day in May - Mark Danielewski
+					The Familiar, Vol. One: One Rainy Day in May - <a href=<?php echo "\"". $amazon["mark"] . "\", " . $tab; ?>>Mark Danielewski</a>
 				</p>
       </li>
       <li id="heinlein">
-        Starship Troopers - Robert Heinlein
+        Starship Troopers - <a href=<?php echo "\"". $amazon["heinlein"] . "\", " . $tab; ?>>Robert Heinlein</a>
       </li>
     </ul>
     <h3>Graphic Novels</h3>
     <ul>
       <li id="moore1">
-        Watchmen - Alan Moore
+        Watchmen - <a href=<?php echo "\"". $amazon["watch"] . "\", " . $tab; ?>>Alan Moore</a>
       </li>
       <li id="moore">
-        V for Vendetta - Alan Moore
+        V for Vendetta - <a href=<?php echo "\"". $amazon["v"] . "\", " . $tab; ?>>Alan Moore</a>
       </li>
     </ul>
     <h3>Western</h3>
     <ul>
       <li id="portis">
-        True Grit - Charles Portis
+        True Grit - <a href=<?php echo "\"". $amazon["portis"] . "\", " . $tab; ?>>Charles Portis</a>
       </li>
       <li id="proulx">
-        Close Range: Wyoming Stories - Annie Proulx
+        Close Range: Wyoming Stories - <a href=<?php echo "\"". $amazon["proulx"] . "\", " . $tab; ?>>Annie Proulx</a>
       </li>
       <li id="alexie">
-        The Lone Ranger and Tonto Fistfight in Heaven - Sherman Alexie
+        The Lone Ranger and Tonto Fistfight in Heaven - <a href=<?php echo "\"". $amazon["alexie"] . "\", " . $tab; ?>>Sherman Alexie</a>
       </li>
     </ul>
 	</section>
   <section>
     <?php
-      foreach ($_GET as $key => $value) {
-        echo "<p>" . $key . " - " . $value . "</p>";
-      }
-      echo "<p>Title exists? " . array_key_exists("Title", $_GET) . "</p>";
-      echo "<p>Title is set?" . empty($_GET["Title"]) . "</p>";
-      echo "<p>Title: " . $_GET["Title"] . "</p>";
-    
       // add form submissions to the db
-      // assume all form fields are completed
+      // assume all form fields are completed -- validated by js
       if (!empty($_GET)):
         $insert = "insert into books (title, genre, author_firstName, author_lastName) values ('" . $_GET["Title"] . "', '" . $_GET["Genre"] . "', '" . $_GET["Author_First_Name"] . "', '" .$_GET["Author_Last_Name"] ."')";
         echo $insert;
         
         echo "Inserting form data into db";
         
-        if ($connection->query($insert) === true):
-          echo "Data inserted";
-        else:
-          echo "Data failed to insert " . $connection->error;
-        endif;
+        $connection->query($insert);
       endif;
       
       echo "<h1>Here are the books already suggested by other visitors</h1>";
@@ -154,7 +156,7 @@
       endif;
     ?>
   </section>
-	<section id="mailingList">
+	<section id="bookSubmission">
     <p id="errorMessage"></p>
 		<h1>Read any good books lately? Add them to the list here!</h1>
 		<form action="index.php" method="get" id="contactList" onsubmit="return verify()">
